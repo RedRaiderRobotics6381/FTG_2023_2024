3b770fdc758224ca0a0dc568908b6e7799c7e2d6
wtarkowski <26tarkowski104@sasd.net>
2023-10-04 14:45:41 -0500

    Added Webcam test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumOpMode.java
@@ -28,8 +28,8 @@ public class MechanumOpMode extends LinearOpMode {
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
-            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double y = gamepad1.left_stick_y * 0.50; // Remember, Y stick value is reversed
+            double x = -gamepad1.left_stick_x * 0.65; // Counteract imperfect strafing
             double rx = gamepad1.right_stick_x;
 
             // Denominator is the largest motor power (absolute value) or 1
