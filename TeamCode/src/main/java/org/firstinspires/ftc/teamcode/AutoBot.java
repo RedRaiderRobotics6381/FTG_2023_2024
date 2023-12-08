f6a583b32b89f4cb1715a0612e8e69eb6858c1ae
wtarkowski <26tarkowski104@sasd.net>
2023-12-07 19:09:56 -0600

    First Auto = done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBot.java
@@ -6,4 +6,11 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 @Autonomous
 public class AutoBot extends FO_MechanumOpMode{
     Auto DriveTrain = new Auto();
-}
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DriveTrain = new Auto();
+        waitForStart();
+
+        if (isStopRequested()) return;
+        DriveTrain.Drive("Forward", 5);
+}}
